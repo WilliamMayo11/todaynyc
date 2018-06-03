f7f9f539cf71df3e36769e5bdebe8d0a08d7f732
William Mayo <wlmayo@gmail.com>
2018-06-02 21:07:01 -0400

    refactor

--- src/models/weather.js
+++ src/models/weather.js
@@ -4,8 +4,7 @@ export class Weather {
   constructor() {
     // this.weatherApi = this.getWeather();
     this.hello = 'hello';
-    this.API_URL = //hidden
-    this.API_KEY = //hidden
+
   }
 
   getWeather() {
