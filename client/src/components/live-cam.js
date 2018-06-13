8d01fdf7c297f343a21a4d17134d50cbf0f5de08
William Mayo <wlmayo@gmail.com>
2018-06-13 19:29:06 -0400

    add carousel

--- client/src/components/live-cam.js
+++ client/src/components/live-cam.js
@@ -1,70 +1,118 @@
 import React, { Component, Fragment } from 'react';
 import '../css/live-cam.css';
+import { Carousel } from 'react-responsive-carousel';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-let videoArr = [
-  "https://www.youtube.com/embed/la90mA4VLa4?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1",
-  "https://www.youtube.com/embed/hck5LT3HtVE?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1",
-  "https://www.youtube.com/embed/xGAseSEdcyc?autoplay=1&controls=0&showinfo=0mute=1?rel=0"
-]
-let videoIndex = 0;
+// let videoArr = [
+//   "https://www.youtube.com/embed/la90mA4VLa4?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1",
+//   "https://www.youtube.com/embed/hck5LT3HtVE?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1",
+//   "https://www.youtube.com/embed/xGAseSEdcyc?autoplay=1&controls=0&showinfo=0mute=1?rel=0"
+// ]
+// let videoIndex = 0;
 
 class LiveCam extends Component {
 
   constructor(props) {
   super(props)
   this.state = {
-    currentVideo: videoArr[0],
+    currentVideo: "https://www.youtube.com/embed/la90mA4VLa4?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1",
+    // display: {display: 'inline-block'},
   }
 }
 
   componentDidMount() {
-    this.switchVideo()
+    // this.switchVideo();
   }
 
-  switchVideo() {
-    setInterval(() => {
-      switch (this.state.currentVideo) {
-        case "https://www.youtube.com/embed/la90mA4VLa4?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1":
-        console.log('videoIndex: ', videoIndex)
-          videoIndex++;
-          this.setState({
-            currentVideo: videoArr[videoIndex]
-          })
-          return;
-        case "https://www.youtube.com/embed/hck5LT3HtVE?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1":
-        console.log('videoIndex: ', videoIndex)
-          videoIndex++;
-          this.setState({
-            currentVideo: videoArr[videoIndex]
-          })
-          return;
-        case "https://www.youtube.com/embed/xGAseSEdcyc?autoplay=1&controls=0&showinfo=0mute=1?rel=0":
-        console.log('videoIndex: ', videoIndex)
-          videoIndex = 0;
-          this.setState({
-            currentVideo: videoArr[videoIndex]
-          })
-          return videoIndex;
-        default:
-          videoIndex = 0;
-      }
-    }, 30000);
-  }
+  // switchVideo() {
+  //   setInterval(() => {
+  //     switch (this.state.currentVideo) {
+  //       case "https://www.youtube.com/embed/la90mA4VLa4?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1":
+  //       console.log('videoIndex: ', videoIndex)
+  //         videoIndex++;
+  //         this.setState({
+  //           currentVideo: videoArr[videoIndex]
+  //         })
+  //         return;
+  //       case "https://www.youtube.com/embed/hck5LT3HtVE?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1":
+  //       console.log('videoIndex: ', videoIndex)
+  //         videoIndex++;
+  //         this.setState({
+  //           currentVideo: videoArr[videoIndex]
+  //         })
+  //         return;
+  //       case "https://www.youtube.com/embed/xGAseSEdcyc?autoplay=1&controls=0&showinfo=0mute=1?rel=0":
+  //       console.log('videoIndex: ', videoIndex)
+  //         videoIndex = 0;
+  //         this.setState({
+  //           currentVideo: videoArr[videoIndex]
+  //         })
+  //         return videoIndex;
+  //       default:
+  //         videoIndex = 0;
+  //     }
+  //   }, 30000);
+  // }
+
+  // toggleDisplay() {
+  //   console.log('toggleDisplay')
+  //   this.setState({
+  //     display: {display: 'none'}
+  //   });
+  // }
 
 
-// <div className="transparent"></div>
+  // <div>
+    // <iframe
+    //   src="https://www.youtube.com/embed/la90mA4VLa4?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1"
+    //   frameBorder="0"
+    //   allow="autoplay; encrypted-media"
+    //   modestbranding="0"
+    // >
+    // </iframe>
+  // </div>
+  // <div>
+    // <iframe
+    //   src="https://www.youtube.com/embed/hck5LT3HtVE?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1"
+    //   frameBorder="0"
+    //   allow="autoplay; encrypted-media"
+    //   modestbranding="0"
+    // >
+    // </iframe>
+  // </div>
 
   render() {
     return(
       <Fragment>
-        <div className="intrinsic-container intrinsic-container-16x9">
-          <iframe
-            src={this.state.currentVideo}
-            frameBorder="0"
-            allow="autoplay; encrypted-media"
-            modestbranding="0"
+        <div>
+          <Carousel
+            showThumbs={false}
+            swipeable={true}
+            emulateTouch={true}
           >
-          </iframe>
+            <div className="intrinsic-container intrinsic-container-16x9">
+              <iframe
+                src="https://www.youtube.com/embed/la90mA4VLa4?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1"
+                frameBorder="0"
+                allow="autoplay; encrypted-media"
+                modestbranding="0"
+                title="brooklyn-bridge"
+              >
+              </iframe>
+              <p className="legend">Legend 1</p>
+            </div>
+            <div className="intrinsic-container intrinsic-container-16x9">
+              <iframe
+                src="https://www.youtube.com/embed/hck5LT3HtVE?rel=0&amp;controls=0&amp;showinfo=0&playsinline=1"
+                frameBorder="0"
+                allow="autoplay; encrypted-media"
+                modestbranding="0"
+                title="downtown"
+              >
+              </iframe>
+              <p className="legend">Legend 2</p>
+            </div>
+          </Carousel>
         </div>
       </Fragment>
     )
--- client/src/models/weather.js
+++ client/src/models/weather.js
@@ -6,23 +6,3 @@ export class Weather {
 }
 
 export default Weather;
-
-
-
-
-
-
-// export class Weather {
-//
-//   constructor() {
-//     this.API_URL = 'http://api.openweathermap.org/data/2.5/weather?';
-//     this.API_KEY = 'e1b97b1617ae2968c4f0ede12310797c';
-//
-//   }
-//
-//   getWeather() {
-//     return fetch(`${this.API_URL}q=${'new york'}&units=imperial&APPID=${this.API_KEY}`)
-//   }
-// }
-//
-// export default Weather;
